c65653595008d794fad89f345e3368a0fdf1c31c
Bojan <bojan.buic@gmail.com>
2015-12-08 21:29:53 +0100

    Razdvojiti DNT aplikaciju na servis i klijenta #1

---
+++ DNT/Racunalo/DNTv2/DNTServiceTest/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using DNTServiceProcessor;
+
+namespace DNTServiceTest
+{
+    public partial class Form1 : Form
+    {
+        private Processor _processor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _processor = new Processor();
+            _processor.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _processor.Stop();
+        }
+    }
+}
