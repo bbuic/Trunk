100b6131eb48b1a9d1eaf074e51e1f5f299fd0c2
Bojan <bojan.buic@gmail.com>
2015-12-15 13:36:34 +0100

    Razdvojiti DNT aplikaciju na servis i klijenta #1

---
+++ DNT/Racunalo/DNTv2/DNTv2/Data/DataService/DogadajDataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using DNTDataAccess;
+using DNTv2.DataModel;
+
+namespace DNTv2.Data.DataService
+{
+    public class DogadajDataService : DNTDataAccess.DataServices.DogadajDataService
+    {
+        public IList<DogadajModel> DajSveDogadaje()
+        {
+            using (OleDbConnection connection = new OleDbConnection(DNTDataAccess.Utils.ReadSetting("ConnectionString")))
+            {
+                DataTable table = new DataTable();
+                OleDbCommand command = new OleDbCommand("SELECT DogadajTipId, DatumOd, DatumDo, Kartica, Ime, Prezime FROM " +
+                                                        "(Dogadaj d LEFT JOIN Kartice ka ON ka.Broj = d.Kartica) " +
+                                                        "LEFT JOIN DNTKorisnici ko ON ka.VlasnikID = ko.ID " +
+                                                        "WHERE DatumDo IS NULL", connection);
+                try
+                {
+                    connection.Open();
+                    new OleDbDataAdapter(command).Fill(table);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                IList<DogadajModel> list = ToList<DogadajModel>(GetArrayList(table));                
+                return list;
+            }
+        }
+
+        public override Type ObjectType
+        {
+            get { return typeof (DogadajModel); }
+        }
+    }
+}
---
+++ DNT/Racunalo/DNTv2/DNTv2/DataModel/DogadajModel.cs
@@ -0,0 +1,27 @@
+﻿using DNTDataAccess;
+
+namespace DNTv2.DataModel
+{
+    public class DogadajModel : Dogadaj
+    {        
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        
+        public string Naziv
+        {
+            get
+            {
+                switch (DogadajTipId)
+                {
+                    case DogadajTip.Foto:
+                        return "Blokada fotosenzora";
+                    case DogadajTip.Vrata:
+                        return "Vanjska vrata trezora otvorena bez aktivnosti predaje pologa";
+                    default:
+                        return "";
+                }
+            }
+            set{}
+        }
+    }
+}
